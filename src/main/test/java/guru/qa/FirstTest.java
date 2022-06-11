941968ff72d7c530c7889836370ffb37138e4df7
Anna Gau <tmp.a@rambler.ru>
2022-06-11 16:09:11 +0300

    added .gitignore

---
+++ src/main/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,17 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    void FirstTest()
+    {
+        Assertions.assertTrue(4>=2);
+    }
+    @Test
+    void SecondTest()
+    {
+        Assertions.assertTrue(3>=2);
+    }
+}
